2008887b5baea233953209a6c4b6795c49db9b21
Sarah <sscannell@my365.bellevue.edu>
2021-09-04 12:15:02 -0400

    Added comments

--- Module_1/HelloWorldApp/ScannellHelloWorld.java
+++ Module_1/HelloWorldApp/ScannellHelloWorld.java
@@ -1,7 +1,26 @@
-class ScannellHelloWorld {
-    public static void main(String[] args) {
-        String firstname = "Sarah", lastname = "Scannell";
+/* 
 
-        System.out.printf("Hello World from %s %s.%n", firstname, lastname);
-    }
-}
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+    Comprehensive Version (12th ed.). Pearson Education, Inc.
+
+Scannell, S. (2021) CIS 505 Intermediate Java Programming. Bellevue University.
+
+*/
+
+class ScannellHelloWorld {
+
+    /**
+     * Method with two arguments, which are used to print the user's first and last name.
+     * @param firstName String
+     * @param lastName String
+     * @return prints the string to the console window.
+     */
+
+    public static void helloWorld(String firstName, String lastName) {
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    } // end helloWorld
+
+    public static void main(String[] args) {
+        helloWorld("Sarah", "Scannell"); // Call helloWorld method passing the values "Sarah" and "Scannell"
+    } // end main
+} // end ScannellHelloWorld
